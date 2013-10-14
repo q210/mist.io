c204e2e9066e07c8169ffbeaf619f7f7a28a3f42
Michail Ballis <michail.ballis@gmail.com>
2013-10-14 22:48:14 +0300

    Link to image URL

--- src/mist/io/static/js/app/views/user_menu.js
+++ src/mist/io/static/js/app/views/user_menu.js
@@ -11,7 +11,7 @@ define('app/views/user_menu', [
     function(user_menu_html) {
         return Ember.View.extend({
             tagName: false,
-            gravatarURL: 'https://www.gravatar.com/avatar/'+md5(EMAIL)+'?d=https%3A%2F%2Fmist.io%2Fcore%2Fmavatar.png&size=16',
+            gravatarURL: 'https://www.gravatar.com/avatar/'+md5(EMAIL)+'?d=http%3A%2F%2Fcharlesandbronson.com%2Fgravatars%2Favatar-25.png&size=16',
             email: EMAIL,
             account_url: URL_PREFIX + '/account',
             logout: URL_PREFIX.length == 0 ? true : false,
